1b70b3f125eb3f003910643343d64fd99769705b
Elazar Gershuni <elazarg@gmail.com>
2021-07-08 16:31:19 +0300

    README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+GENDER-DOTS, a challenge set comparing male/female diacritization in Hebrew
+
+The dataset is in `data/expected`.
+
+To run the experiment, first clone the repository and `cd` into it.
+The files are in `tsv` format (tab-separated values), with the following columns:
+* `id`, index of the example in the file
+* `bookid`, a unique id for the book (across files)
+* `author`, the name of the author of the book (usually in Hebrew)
+* `book`, the name of the book (usually in Hebrew)
+* `word`, the target word that must be classified correctly (diacritized as male/female)
+* `index`, the index of the word in both sentences
+* `masculine`, a sentence in which the target word is masculine
+* `feminine`, a sentence in which the target word is feminine
+* `success_masculine`, 1 if the diacritization of `masculine` is correct, 0 otherwise
+* `success_masculine`, 1 if the diacritization of `feminine` is correct, 0 otherwise
+
+Then, install the requirements:
+```shell
+python -m pip install -r requirements.txt
+```
--- src/run_experiment.py
+++ src/run_experiment.py
@@ -1,10 +1,12 @@
+import argparse
 from pathlib import Path
+import logging
 
 from hebrew import remove_niqqud
-
 import external_apis
 import utils
-import argparse
+
+logging.getLogger().setLevel(logging.INFO)
 
 
 def run_experiment(filename, system):
@@ -18,6 +20,7 @@ def run_experiment(filename, system):
 
     with open(outfile, 'w', encoding='utf8') as f:
         for i, bookid, author, book, word, index, expected_male, expected_female, original in rows:
+            logging.info(i)
             index = int(index)
 
             actual_male = fetch(remove_niqqud(expected_male))
@@ -25,6 +28,8 @@ def run_experiment(filename, system):
 
             success_male = int(expected_male.split()[index] == actual_male.split()[index])
             success_female = int(expected_female.split()[index] == actual_female.split()[index])
+
+            logging.info("done")
 
             print(i, bookid, author, book, word, index, actual_male, actual_female, original, success_male, success_female,
                   file=f, sep='\t')
